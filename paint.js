e9d0f0c5cda19bec4ee676e192687898df40dfe6
E-Wickham <ericwickham@hotmail.com>
2025-04-15 00:50:05 -0400

    article card changes

--- paint.js
+++ paint.js
@@ -1040,26 +1040,6 @@ const candStories =
    "publication": "PressProgress",
    "link": "https://pressprogress.ca/conservative_mp_agrees_100_with_ben_carson_absurd_rnc_speech_denouncing_the_secular_agenda/"
  },
- {
-   "riding_name": "Cariboo—Prince George",
-   "riding_id": 301,
-   "candidate": "Todd Doherty",
-   "party": "Conservative",
-   "news_story": "More Than Half of Andrew Scheer’s Elected Conservative MPs Were Supported by Anti-Abortion Groups",
-   "pub_date": "2019-10-25",
-   "publication": "PressProgress",
-   "link": "https://pressprogress.ca/more-than-half-of-andrew-scheers-elected-conservative-mps-were-supported-by-anti-abortion-groups/"
- },
- {
-   "riding_name": "Cariboo—Prince George",
-   "riding_id": 301,
-   "candidate": "Todd Doherty",
-   "party": "Conservative",
-   "news_story": "Here is a List of 67 Conservative Candidates Who Will Vote Against Women’s Reproductive Rights",
-   "pub_date": "2019-10-16",
-   "publication": "PressProgress",
-   "link": "https://pressprogress.ca/here-is-a-list-of-67-conservative-candidates-who-will-vote-against-womens-reproductive-rights/"
- }
 ]
 
 
@@ -3525,6 +3505,8 @@ function createSubmitLink(target) {
     link.setAttribute("target", '_blank')
     link.appendChild(submitBtn)
     submitMsg.appendChild(submitDiv)
+    submitMsg.setAttribute("class", "submit-div")
+
     submitMsg.appendChild(link)
     target.appendChild(submitMsg)
 
